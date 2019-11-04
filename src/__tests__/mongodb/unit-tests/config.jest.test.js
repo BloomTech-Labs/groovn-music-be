afb70c82f139629c20439a24cdc6e29d734f868a
Dominic Torruellas <dom@domtorr.com>
2019-11-03 20:55:36 -0700

    initial set up of Jest testing suite

---
+++ src/__tests__/mongodb/unit-tests/config.jest.test.js
@@ -0,0 +1,24 @@
+import mongoose from 'mongoose';
+import testDb from '../../../util/testDb';
+
+// Creating a fake mongo model to test if Jest is working
+const testSchema = new mongoose.Schema({
+  test: { type: Boolean, required: true },
+});
+
+const testModel = new mongoose.model('Test', testSchema);
+
+describe('Jest configuration test', () => {
+  beforeAll(() => testDb.connect());
+
+  afterAll(() => testDb.disconnect());
+
+  it('create & save Jest test data successfully', async () => {
+    const validTest = new testModel({ test: true });
+    const savedTest = await validTest.save();
+
+    // Object ID should be defined when successfully saved to MongoDB
+    expect(savedTest._id).toBeDefined();
+    expect(savedTest.test).toBe(true);
+  });
+});
---
+++ src/util/testDb.js
@@ -0,0 +1,23 @@
+import mongoose from 'mongoose';
+
+export default {
+  connect: async () => {
+    await mongoose.connect(
+      process.env.MONGO_URL,
+      {
+        useUnifiedTopology: true,
+        useNewUrlParser: true,
+        useCreateIndex: true,
+      },
+      err => {
+        if (err) {
+          console.error(err);
+          process.exit(1);
+        }
+      }
+    );
+  },
+  disconnect: async () => {
+    await mongoose.disconnect();
+  },
+};
