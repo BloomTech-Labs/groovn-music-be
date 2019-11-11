e4a013caeae411520a25e4bb4fe94e5107d402f7
unknown <jjamal95@gmail.com>
2019-11-11 17:46:51 -0600

    working on playlist model

---
+++ src/models/Playlist.js
@@ -0,0 +1,34 @@
+import mongoose from "mongoose";
+
+const PlayListSchema =  new mongoose.Schema({
+    key: { // playlist_id
+        type: Number
+    },
+    collaborative: Boolean,     // Returns true only if user modification is allowed by owner. Otherwise returns false.
+    description: String,    // Only for modified playlist, otherwise null 
+    name: String, //  Name of playlist
+    id: String,        // The spotify ID for the playlist
+    owner: {     // The user who owns the playlist
+        type: ObjectId,  // Public user object 
+        ref: "User"
+    },
+    tracks: {
+        description: Array, 
+        typa: ObjectId,  
+        ref: "Track"
+    },
+    public: {
+        type: Boolean || null   // True if the playlist is public, false the playlist is private. Null the playlist status is not relevant. 
+    },
+    type: {
+        description: String,
+    },
+
+    uri: {
+        description: String  // The Spotify URI (Uniform Resource Indicator) for the playlist 
+    },
+    
+});
+
+const Playlist = mongoose.model('Playlist', PlayListSchema);
+export default Playlist;
