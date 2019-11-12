9a96525109228952bc435b949be9b863daa71a3b
Douglas Campbell <dacampbelltbone@gmail.com>
2019-11-12 00:25:18 -0500

    Fixed User model, createUser resolver and User typeDef and mutation.

--- src/graphql/schema/User/user.js
+++ src/graphql/schema/User/user.js
@@ -1,13 +1,22 @@
-import {
-  gql
-} from 'apollo-server-express';
+import { gql } from 'apollo-server-express';
 import User from '../../../models/User/user';
 
 // Type definitions for User go here
-export const typeDefs = gql `
+export const typeDefs = gql`
   extend type Query {
     users: [User!]!
   }
+
+  extend type Mutation {
+    createUser(
+      firstName: String!
+      lastName: String
+      email: String!
+      password: String!
+      displayName: String!
+    ): User
+  }
+
   type User {
     firstName: String!
     lastName: String!
@@ -24,21 +33,18 @@ export const resolvers = {
     users: () => ['Bob', 'Jill'],
   },
   Mutation: {
-    createUser: (_, {
-      email,
-      password,
-      firstName,
-      lastName,
-      displayName
-    }) => {
+    createUser: async (
+      _,
+      { email, password, firstName, lastName, displayName }
+    ) => {
       const newUser = new User({
         email,
         password,
         firstName,
         lastName,
-        displayName
-      })
-      newUser.save();
-    }
-  }
+        displayName,
+      });
+      return await newUser.save();
+    },
+  },
 };
