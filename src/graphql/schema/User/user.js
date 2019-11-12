db61ec7d9a7fdacdc1becc9a550e39c9196dfa10
unknown <dj.zaragoza@icloud.com>
2019-11-11 20:33:49 -0800

    fixed some of my extensions to ensure formatting is set correctly for GraphQL

--- src/graphql/schema/User/user.js
+++ src/graphql/schema/User/user.js
@@ -1,13 +1,16 @@
-import { gql } from 'apollo-server-express';
+import {
+  gql
+} from 'apollo-server-express';
 import User from '../../../models/User/user';
 
 // Type definitions for User go here
-export const typeDefs = gql`
+export const typeDefs = gql `
   extend type Query {
     users: [User!]!
   }
   type User {
-    username: String!
+    firstName: String!
+    lastName: String!
     id: ID!
     email: String!
     password: String!
@@ -19,8 +22,23 @@ export const typeDefs = gql`
 export const resolvers = {
   Query: {
     users: () => ['Bob', 'Jill'],
-  }, 
+  },
   Mutation: {
-    createUser: (_, { email, password } ) => 
+    createUser: (_, {
+      email,
+      password,
+      firstName,
+      lastName,
+      displayName
+    }) => {
+      const newUser = new User({
+        email,
+        password,
+        firstName,
+        lastName,
+        displayName
+      })
+      newUser.save();
+    }
   }
 };
